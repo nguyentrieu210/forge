b054a55e404518b70d79fa0aa9aa553025526e13
nguyentrieu210 <trieu.nt93@gmail.com>
2026-07-30 23:46:40 +0700

    feat(procurement): use allocation-aware mutation store

--- server/apps/tenant-worker/src/aggregate-do.ts
+++ server/apps/tenant-worker/src/aggregate-do.ts
@@ -4,7 +4,7 @@ import { createO2CControllerRegistry } from "../../../packages/clouderp-selling/
 import { registerErpCoreControllers } from "../../../packages/clouderp-core/src/index.js";
 import { registerStockControllers } from "../../../packages/clouderp-stock/src/index.js";
 import { registerErpNextCoreControllers } from "../../../packages/clouderp-erpnext/src/index.js";
-import { D1MutationStore, DocumentKernel } from "../../../packages/document-kernel/src/index.js";
+import { D1PurchaseAllocationMutationStore, DocumentKernel } from "../../../packages/document-kernel/src/index.js";
 import { D1DocumentAccessStore, D1MetadataStore, GenericMetadataController, MetadataPermissionService } from "../../../packages/frappe-model/src/index.js";
 import type { TenantEnv } from "./env.js";
 
@@ -15,7 +15,7 @@ export class AggregateCoordinator extends DurableObject<TenantEnv> {
     super(ctx, env);
     const metadata = new D1MetadataStore(env.DB);
     const registry = registerErpNextCoreControllers(registerStockControllers(registerErpCoreControllers(createO2CControllerRegistry()))).setFallback(new GenericMetadataController(metadata));
-    this.kernel = new DocumentKernel(registry, new D1MutationStore(env.DB), new MetadataPermissionService(metadata, undefined, new D1DocumentAccessStore(env.DB)));
+    this.kernel = new DocumentKernel(registry, new D1PurchaseAllocationMutationStore(env.DB), new MetadataPermissionService(metadata, undefined, new D1DocumentAccessStore(env.DB)));
   }
 
   async mutate<T extends JsonObject>(command: MutationCommand<T>): Promise<MutationReceipt> {
