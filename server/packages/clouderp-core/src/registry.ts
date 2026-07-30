73433d518abb8fbb809e1ea3b3d33479a787bd5b
nguyentrieu210 <trieu.nt93@gmail.com>
2026-07-31 00:18:11 +0700

    feat(procurement): register FIFO allocation controllers

--- server/packages/clouderp-core/src/registry.ts
+++ server/packages/clouderp-core/src/registry.ts
@@ -1,6 +1,15 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
-import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, PurchaseOrderController, PurchaseReceiptController, RequestForQuotationController, StockEntryController, SupplierQuotationController } from "./controllers.js";
+import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, RequestForQuotationController, StockEntryController, SupplierQuotationController } from "./controllers.js";
+import { AllocatingPurchaseOrderController, AllocatingPurchaseReceiptController } from "./purchase-allocation-controllers.js";
 
 export function registerErpCoreControllers(registry: ControllerRegistry): ControllerRegistry {
-  return registry.register(new JournalEntryController()).register(new MaterialRequestController()).register(new RequestForQuotationController()).register(new SupplierQuotationController()).register(new PurchaseOrderController()).register(new PurchaseReceiptController()).register(new PurchaseInvoiceController()).register(new StockEntryController());
+  return registry
+    .register(new JournalEntryController())
+    .register(new MaterialRequestController())
+    .register(new RequestForQuotationController())
+    .register(new SupplierQuotationController())
+    .register(new AllocatingPurchaseOrderController())
+    .register(new AllocatingPurchaseReceiptController())
+    .register(new PurchaseInvoiceController())
+    .register(new StockEntryController());
 }
