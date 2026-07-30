9f708317a3a3480c0603ec2a7857a2294ed6bdd3
nguyentrieu210 <trieu.nt93@gmail.com>
2026-07-31 00:18:26 +0700

    feat(procurement): export allocation controllers

--- server/packages/clouderp-core/src/index.ts
+++ server/packages/clouderp-core/src/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./uom.js";
 export * from "./controllers.js";
 export * from "./purchase-allocation.js";
+export * from "./purchase-allocation-controllers.js";
 export * from "./registry.js";
