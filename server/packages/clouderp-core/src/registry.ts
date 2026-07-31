12e6dddb795cf6192aef3dd853e6b8ed5a650ced
nguyentrieu210 <trieu.nt93@gmail.com>
2026-07-31 15:55:50 +0700

    fix(purchase): use settlement lifecycle controller

--- server/packages/clouderp-core/src/registry.ts
+++ server/packages/clouderp-core/src/registry.ts
@@ -1,7 +1,8 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
 import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, RequestForQuotationController, StockEntryController, SupplierQuotationController } from "./controllers.js";
-import { PurchaseAllocationOverrideController, PurchaseSettlementController } from "./purchase-allocation-action-controllers.js";
+import { PurchaseAllocationOverrideController } from "./purchase-allocation-action-controllers.js";
 import { RolloutPurchaseOrderController, RolloutPurchaseReceiptController } from "./purchase-allocation-rollout-controllers.js";
+import { PurchaseSettlementLifecycleController } from "./purchase-settlement-lifecycle-controller.js";
 
 export function registerErpCoreControllers(registry: ControllerRegistry): ControllerRegistry {
   return registry
@@ -11,7 +12,7 @@ export function registerErpCoreControllers(registry: ControllerRegistry): Contro
     .register(new SupplierQuotationController())
     .register(new RolloutPurchaseOrderController())
     .register(new RolloutPurchaseReceiptController())
-    .register(new PurchaseSettlementController())
+    .register(new PurchaseSettlementLifecycleController())
     .register(new PurchaseAllocationOverrideController())
     .register(new PurchaseInvoiceController())
     .register(new StockEntryController());
