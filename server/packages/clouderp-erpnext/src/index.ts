2e6e7e0eb9d274d0ed8d9dbb7f8fd4797381be00
nguyentrieu210 <trieu.nt93@gmail.com>
2026-07-31 19:46:32 +0700

    chore(manufacturing): export work order stock-UOM guard

--- server/packages/clouderp-erpnext/src/index.ts
+++ server/packages/clouderp-erpnext/src/index.ts
@@ -4,6 +4,7 @@ export * from "./controllers.js";
 export * from "./physical-stock-entry.js";
 export * from "./manufacturing-lifecycle.js";
 export * from "./manufacturing-stock-guard.js";
+export * from "./manufacturing-work-order-guard.js";
 export * from "./registry.js";
 export * from "./suite-controllers.js";
 export * from "./enterprise-types.js";
