652431c3060dfd795f6d50f5d2d8b9a599a0a5f9
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-01 06:35:20 +0700

    feat(finance): register allocation-aware payment controllers

--- server/packages/clouderp-selling/src/registry.ts
+++ server/packages/clouderp-selling/src/registry.ts
@@ -1,11 +1,12 @@
-
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
-import { DeliveryNoteController, PaymentEntryController, SalesInvoiceController, SalesOrderController } from "./controllers.js";
+import { DeliveryNoteController, SalesInvoiceController, SalesOrderController } from "./controllers.js";
+import { FinancePaymentEntryController, PaymentAllocationController } from "./finance-controllers.js";
 
 export function createO2CControllerRegistry(): ControllerRegistry {
   return new ControllerRegistry()
     .register(new SalesOrderController())
     .register(new DeliveryNoteController())
     .register(new SalesInvoiceController())
-    .register(new PaymentEntryController());
+    .register(new FinancePaymentEntryController())
+    .register(new PaymentAllocationController());
 }
