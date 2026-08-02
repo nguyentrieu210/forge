7689370d70437ef0fb8e13bac87d23bbc3edddaf
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:15:44 +0700

    feat(ws13): export manifest and reconciliation contracts

--- server/packages/migration/src/public.ts
+++ server/packages/migration/src/public.ts
@@ -2,4 +2,6 @@ export * from "./index.js";
 export * from "./adapters.js";
 export * from "./execution.js";
 export * from "./implementation.js";
+export * from "./manifest.js";
+export * from "./reconcile.js";
 export * from "./template.js";
