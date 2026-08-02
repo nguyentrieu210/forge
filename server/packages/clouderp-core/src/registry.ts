f4d45ae6763752227501037b6a25a1554712fd72
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 01:38:39 +0700

    fix(accounting): register policy-aware purchase receipt

--- server/packages/clouderp-core/src/registry.ts
+++ server/packages/clouderp-core/src/registry.ts
@@ -1,8 +1,9 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
 import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, RequestForQuotationController, SupplierQuotationController } from "./controllers.js";
+import { AccountingPurchaseReceiptController } from "./accounting-purchase-receipt-controller.js";
 import { AccountingStockEntryController } from "./accounting-stock-entry-controller.js";
 import { PurchaseAllocationOverrideController } from "./purchase-allocation-action-controllers.js";
-import { RolloutPurchaseOrderController, RolloutPurchaseReceiptController } from "./purchase-allocation-rollout-controllers.js";
+import { RolloutPurchaseOrderController } from "./purchase-allocation-rollout-controllers.js";
 import { PurchaseSettlementLifecycleController } from "./purchase-settlement-lifecycle-controller.js";
 
 export function registerErpCoreControllers(registry: ControllerRegistry): ControllerRegistry {
@@ -12,7 +13,7 @@ export function registerErpCoreControllers(registry: ControllerRegistry): Contro
     .register(new RequestForQuotationController())
     .register(new SupplierQuotationController())
     .register(new RolloutPurchaseOrderController())
-    .register(new RolloutPurchaseReceiptController())
+    .register(new AccountingPurchaseReceiptController())
     .register(new PurchaseSettlementLifecycleController())
     .register(new PurchaseAllocationOverrideController())
     .register(new PurchaseInvoiceController())
