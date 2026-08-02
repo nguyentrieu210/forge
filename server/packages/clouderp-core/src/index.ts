cd2c5a8dbb0e0637601f95a8fcc7a1ad122537f1
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:22:59 +0700

    feat(ws03): export supplier procurement policy

--- server/packages/clouderp-core/src/index.ts
+++ server/packages/clouderp-core/src/index.ts
@@ -3,6 +3,7 @@ export * from "./uom.js";
 export * from "./controllers.js";
 export * from "./procurement-decisions.js";
 export * from "./procurement-integrity-controllers.js";
+export * from "./supplier-policy.js";
 export * from "./purchase-allocation.js";
 export * from "./purchase-allocation-controllers.js";
 export * from "./purchase-allocation-action-controllers.js";
