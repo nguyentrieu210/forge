4af8fccb42031d814ec25e9424a5fae2a5f03211
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:14:17 +0700

    fix(inventory): register tracked-stock integrity controller

--- server/packages/clouderp-stock/src/registry.ts
+++ server/packages/clouderp-stock/src/registry.ts
@@ -1,9 +1,9 @@
 import type { ControllerRegistry } from "../../document-kernel/src/index.js";
-import { SerialAndBatchBundleController } from "./controllers.js";
 import { RepostItemValuationIntegrityController } from "./repost-integrity.js";
+import { SerialAndBatchBundleIntegrityController } from "./tracking-integrity.js";
 
 export function registerStockControllers(registry: ControllerRegistry): ControllerRegistry {
   return registry
-    .register(new SerialAndBatchBundleController())
+    .register(new SerialAndBatchBundleIntegrityController())
     .register(new RepostItemValuationIntegrityController());
 }
