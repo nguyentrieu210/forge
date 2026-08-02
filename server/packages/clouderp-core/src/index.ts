dbfb8d44312bd654c9888c01e6caaa44aa83cbed
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:35:52 +0700

    feat(ws03): export procurement analytics

--- server/packages/clouderp-core/src/index.ts
+++ server/packages/clouderp-core/src/index.ts
@@ -3,6 +3,7 @@ export * from "./uom.js";
 export * from "./controllers.js";
 export * from "./procurement-decisions.js";
 export * from "./procurement-integrity-controllers.js";
+export * from "./procurement-analytics.js";
 export * from "./supplier-policy.js";
 export * from "./supplier-lifecycle-controllers.js";
 export * from "./purchase-allocation.js";
