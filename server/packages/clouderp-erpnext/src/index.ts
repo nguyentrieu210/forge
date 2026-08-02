a63b9978c9a24a82abf5fe4946bfa76463f2f82e
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-02 16:16:17 +0700

    feat(finance): export warehouse cash controllers

--- server/packages/clouderp-erpnext/src/index.ts
+++ server/packages/clouderp-erpnext/src/index.ts
@@ -14,3 +14,4 @@ export * from "./suite-controllers.js";
 export * from "./enterprise-types.js";
 export * from "./enterprise-controllers.js";
 export * from "./alumdoor-inventory.js";
+export * from "./warehouse-cash.js";
