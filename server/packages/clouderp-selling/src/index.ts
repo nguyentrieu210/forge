0adbf02208d53a3452a223228c4e1c9f55ec79d3
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 02:06:18 +0700

    fix(accounting): export company-currency delivery controller

--- server/packages/clouderp-selling/src/index.ts
+++ server/packages/clouderp-selling/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./controllers.js";
+export * from "./accounting-delivery-note-controller.js";
 export * from "./finance-controllers.js";
 export * from "./registry.js";
 export * from "./totals.js";
