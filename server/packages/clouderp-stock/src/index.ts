55b1f2e08e91aa744008b47e84509c9c6fe150ac
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:20:08 +0700

    chore(wms): export putaway planning primitive

--- server/packages/clouderp-stock/src/index.ts
+++ server/packages/clouderp-stock/src/index.ts
@@ -3,6 +3,7 @@ export * from "./valuation.js";
 export * from "./tracking.js";
 export * from "./tracking-integrity.js";
 export * from "./warehouse-scope.js";
+export * from "./wms-putaway.js";
 export * from "./controllers.js";
 export * from "./repost-integrity.js";
 export * from "./registry.js";
