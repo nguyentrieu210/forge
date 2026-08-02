eec103cdfb5da591bdd9bd5265a2ce5cb664b721
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 06:22:28 +0700

    feat(ws03): register procurement integrity and supplier lifecycle

--- server/packages/clouderp-core/src/registry.ts
+++ server/packages/clouderp-core/src/registry.ts
@@ -1,17 +1,24 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
-import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, RequestForQuotationController, StockEntryController, SupplierQuotationController } from "./controllers.js";
+import { JournalEntryController, MaterialRequestController, PurchaseInvoiceController, StockEntryController } from "./controllers.js";
+import { ProcurementPurchaseOrderController, ProcurementRequestForQuotationController, ProcurementSupplierContractController, ProcurementSupplierQuotationController } from "./procurement-integrity-controllers.js";
 import { PurchaseAllocationOverrideController } from "./purchase-allocation-action-controllers.js";
-import { RolloutPurchaseOrderController, RolloutPurchaseReceiptController } from "./purchase-allocation-rollout-controllers.js";
+import { RolloutPurchaseReceiptController } from "./purchase-allocation-rollout-controllers.js";
 import { PurchaseSettlementLifecycleController } from "./purchase-settlement-lifecycle-controller.js";
+import { SupplierQualificationController, SupplierRatingController } from "./supplier-lifecycle-controllers.js";
+import { SupplierSelectionController } from "./supplier-selection-controller.js";
 
 export function registerErpCoreControllers(registry: ControllerRegistry): ControllerRegistry {
   return registry
     .register(new JournalEntryController())
     .register(new MaterialRequestController())
-    .register(new RequestForQuotationController())
-    .register(new SupplierQuotationController())
-    .register(new RolloutPurchaseOrderController())
+    .register(new ProcurementRequestForQuotationController())
+    .register(new ProcurementSupplierQuotationController())
+    .register(new SupplierSelectionController())
+    .register(new ProcurementPurchaseOrderController())
     .register(new RolloutPurchaseReceiptController())
+    .register(new SupplierQualificationController())
+    .register(new SupplierRatingController())
+    .register(new ProcurementSupplierContractController())
     .register(new PurchaseSettlementLifecycleController())
     .register(new PurchaseAllocationOverrideController())
     .register(new PurchaseInvoiceController())
