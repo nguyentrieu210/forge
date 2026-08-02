d74f4751058ecb908dc12ff590df2ad6add822d2
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 02:06:08 +0700

    fix(accounting): register company-currency Delivery Note controller

--- server/packages/clouderp-selling/src/registry.ts
+++ server/packages/clouderp-selling/src/registry.ts
@@ -1,12 +1,13 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
-import { DeliveryNoteController, SalesInvoiceController, SalesOrderController } from "./controllers.js";
+import { SalesInvoiceController, SalesOrderController } from "./controllers.js";
+import { AccountingDeliveryNoteController } from "./accounting-delivery-note-controller.js";
 import { PaymentAllocationController } from "./finance-controllers.js";
 import { SafeFinancePaymentEntryController } from "./safe-finance-payment-entry.js";
 
 export function createO2CControllerRegistry(): ControllerRegistry {
   return new ControllerRegistry()
     .register(new SalesOrderController())
-    .register(new DeliveryNoteController())
+    .register(new AccountingDeliveryNoteController())
     .register(new SalesInvoiceController())
     .register(new SafeFinancePaymentEntryController())
     .register(new PaymentAllocationController());
