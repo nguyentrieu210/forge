b98ce21da8d9d536faee9fbcdcf812ee97a6e8ed
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:13:24 +0700

    feat(ws03): export procurement decision engine

--- server/packages/clouderp-core/src/index.ts
+++ server/packages/clouderp-core/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./uom.js";
 export * from "./controllers.js";
+export * from "./procurement-decisions.js";
 export * from "./purchase-allocation.js";
 export * from "./purchase-allocation-controllers.js";
 export * from "./purchase-allocation-action-controllers.js";
