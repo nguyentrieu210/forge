a6e3cbd2b80f8d47fd20a08fcbba391304c1af7e
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 05:14:27 +0700

    chore(inventory): export tracking integrity controller

--- server/packages/clouderp-stock/src/index.ts
+++ server/packages/clouderp-stock/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./valuation.js";
 export * from "./tracking.js";
+export * from "./tracking-integrity.js";
 export * from "./controllers.js";
 export * from "./repost-integrity.js";
 export * from "./registry.js";
