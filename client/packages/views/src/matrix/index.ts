61b50e8fa8123ebf33607661ff616a992c0bae5a
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 19:53:46 +0700

    feat(matrix): export metadata driven container

--- client/packages/views/src/matrix/index.ts
+++ client/packages/views/src/matrix/index.ts
@@ -1,3 +1,4 @@
+export { MatrixContainer } from "./MatrixContainer.js";
 export { MatrixRenderer, type MatrixRendererProps } from "./MatrixRenderer.js";
 export * from "./model.js";
 export type * from "./types.js";
