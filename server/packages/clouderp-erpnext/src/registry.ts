f56ff5d806ebd49c33fef46dc24a9e1cf896a298
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-03 18:58:05 +0700

    chore(ws05): reconcile ERP registry with converged main

--- server/packages/clouderp-erpnext/src/registry.ts
+++ server/packages/clouderp-erpnext/src/registry.ts
@@ -5,13 +5,24 @@ import {
 } from "./controllers.js";
 import {
   AssetDisposalController, AssetMaintenanceController, AssetMovementController, ExpenseClaimController,
-  IssueController, JobCardController, PosClosingEntryController, PosInvoiceController, PosOpeningEntryController,
+  IssueController, JobCardController,
   ProductionPlanController, QualityInspectionController, TimesheetController,
 } from "./suite-controllers.js";
+import {
+  HardenedPosClosingEntryController, HardenedPosOpeningEntryController,
+} from "./pos-session-hardening.js";
+import { ExactCancelPosInvoiceController } from "./pos-cancel-exact.js";
+import { DeliveryTripController, ProofOfDeliveryController } from "./logistics-controllers.js";
+import { FreightEstimateController, TransportContractController } from "./freight-controllers.js";
 import {
   BankReconciliationController, BankTransactionController, EInvoiceSubmissionController, PayrollEntryController,
   SubscriptionController,
 } from "./enterprise-controllers.js";
+import {
+  FinanceBudgetCommitmentController,
+  FinanceBudgetController,
+  FinanceBudgetRevisionController,
+} from "./finance-budget.js";
 import {
   EmployeeOnboardingController, EmployeePromotionController, EmployeeSeparationController,
   EmployeeTransferController, EmploymentContractController, InterviewController, JobApplicantController,
@@ -108,11 +119,18 @@ export function registerErpNextCoreControllers(registry: ControllerRegistry): Co
     .register(new GoalController())
     .register(new HrmAppraisalController())
     .register(new TrainingEventController())
-    .register(new PosOpeningEntryController())
-    .register(new PosInvoiceController())
-    .register(new PosClosingEntryController())
+    .register(new HardenedPosOpeningEntryController())
+    .register(new ExactCancelPosInvoiceController())
+    .register(new HardenedPosClosingEntryController())
+    .register(new DeliveryTripController())
+    .register(new ProofOfDeliveryController())
+    .register(new TransportContractController())
+    .register(new FreightEstimateController())
     .register(new BankTransactionController())
     .register(new BankReconciliationController())
+    .register(new FinanceBudgetController())
+    .register(new FinanceBudgetRevisionController())
+    .register(new FinanceBudgetCommitmentController())
     .register(new HrmSalarySlipController())
     .register(new PayrollEntryController())
     .register(new SubscriptionController())
