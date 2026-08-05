0b4b7225276cde955a65d2d2283d092461855398
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-05 11:56:35 +0700

    feat(commerce): export marketplace sync runner

--- server/packages/social-commerce/src/index.ts
+++ server/packages/social-commerce/src/index.ts
@@ -69,3 +69,4 @@ export * from "./marketplace-settlement.js";
 export * from "./provider-order-normalization.js";
 export * from "./provider-order-ingest.js";
 export * from "./marketplace-sync.js";
+export * from "./marketplace-sync-runner.js";
