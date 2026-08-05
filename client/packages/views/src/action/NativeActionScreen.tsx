9c77a83401006bf2e7ddcd470f4ce72d12f18d4f
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-05 13:51:41 +0700

    feat(views): route rich action tables through inline child grid

--- client/packages/views/src/action/NativeActionScreen.tsx
+++ client/packages/views/src/action/NativeActionScreen.tsx
@@ -1,10 +1,14 @@
 /** @jsxImportSource react */
 import { ActionScreen as ExistingActionScreen, type ActionScreenProps } from "./FriendlyActionScreen.js";
 import { preferFirstClassActionInputTables } from "./input-table.js";
+import { isRichAction, RichActionScreen } from "./RichActionScreen.js";
 
 /** Public ActionScreen boundary for the rolling AppAction input-table migration. */
 export function ActionScreen(props: ActionScreenProps) {
-  return <ExistingActionScreen {...props} action={preferFirstClassActionInputTables(props.action)} />;
+  const action = preferFirstClassActionInputTables(props.action);
+  return isRichAction(action)
+    ? <RichActionScreen {...props} action={action} />
+    : <ExistingActionScreen {...props} action={action} />;
 }
 
 export type { ActionScreenProps } from "./ActionScreen.js";
