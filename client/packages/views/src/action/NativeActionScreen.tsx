0d8df384ee4a2dac885d20e03f5cb75b3626feab
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-05 21:59:55 +0700

    feat(views): route metadata master-detail actions

--- client/packages/views/src/action/NativeActionScreen.tsx
+++ client/packages/views/src/action/NativeActionScreen.tsx
@@ -2,10 +2,14 @@
 import { ActionScreen as ExistingActionScreen, type ActionScreenProps } from "./FriendlyActionScreen.js";
 import { preferFirstClassActionInputTables } from "./input-table.js";
 import { isRichAction, RichActionScreen } from "./RichActionScreen.js";
+import { MasterDetailListActionScreen, masterDetailListConfig } from "./MasterDetailListActionScreen.js";
 
 /** Public ActionScreen boundary for the rolling AppAction input-table migration. */
 export function ActionScreen(props: ActionScreenProps) {
   const action = preferFirstClassActionInputTables(props.action);
+  if (masterDetailListConfig(action)) {
+    return <MasterDetailListActionScreen {...props} action={action} />;
+  }
   return isRichAction(action)
     ? <RichActionScreen {...props} action={action} />
     : <ExistingActionScreen {...props} action={action} />;
