4bb79b0b3bb765ad0631b800b7280babeb306531
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-05 10:06:48 +0700

    feat(crm): decorate customer 360 with channel identities

--- server/packages/clouderp-selling/src/registry.ts
+++ server/packages/clouderp-selling/src/registry.ts
@@ -1,7 +1,7 @@
 import { ControllerRegistry } from "../../document-kernel/src/index.js";
 import { CrmChannelPartnerController, CrmFieldCheckInController, CrmSalesRouteController, CrmSalesRouteStopController, CrmSellOutReportController } from "./crm-channel-controllers.js";
 import { CrmActivityController } from "./crm-controllers.js";
-import { CrmCustomer360Controller } from "./crm-customer-360-controller.js";
+import { CrmCustomer360ExternalIdentityController } from "./crm-customer-360-external-identity-controller.js";
 import { CrmContactController, CrmOrganizationController } from "./crm-directory-controllers.js";
 import { CrmCustomerExternalIdentityController } from "./crm-external-identity-controller.js";
 import { CrmConsentAwareMarketingListMemberController } from "./crm-marketing-consent-controller.js";
@@ -21,7 +21,7 @@ export function createO2CControllerRegistry(): ControllerRegistry {
     .register(new CrmTeamAwareLeadController())
     .register(new CrmTeamAwareDealController())
     .register(new CrmActivityController())
-    .register(new CrmCustomer360Controller())
+    .register(new CrmCustomer360ExternalIdentityController())
     .register(new CrmOrganizationController())
     .register(new CrmContactController())
     .register(new CrmCustomerExternalIdentityController())
