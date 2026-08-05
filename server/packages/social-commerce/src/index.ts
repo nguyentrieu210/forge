f928e974aae90606ba24a069a7bee6e10d1b1c23
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-05 22:03:04 +0700

    feat(commerce): export marketplace SLA evaluator

--- server/packages/social-commerce/src/index.ts
+++ server/packages/social-commerce/src/index.ts
@@ -73,5 +73,6 @@ export * from "./marketplace-sync.js";
 export * from "./marketplace-sync-config.js";
 export * from "./marketplace-sync-runner.js";
 export * from "./marketplace-maintenance.js";
+export * from "./marketplace-sla.js";
 export * from "./marketplace-sla-policy-controller.js";
 export * from "./registry.js";
