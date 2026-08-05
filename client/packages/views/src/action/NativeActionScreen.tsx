7aace3669637d9438a801f6d331e8cc4dd8cd57c
nguyentrieu210 <trieu.nt93@gmail.com>
2026-08-06 02:00:31 +0700

    feat(ui): route metadata report and history action renderers

--- client/packages/views/src/action/NativeActionScreen.tsx
+++ client/packages/views/src/action/NativeActionScreen.tsx
@@ -4,12 +4,16 @@ import { preferFirstClassActionInputTables } from "./input-table.js";
 import { isRichAction, RichActionScreen } from "./RichActionScreen.js";
 import { MasterDetailListActionScreen, masterDetailListConfig } from "./MasterDetailListActionScreen.js";
 import { SelectionBatchActionScreen, selectionBatchConfig } from "./SelectionBatchActionScreen.js";
+import { OperationalReportActionScreen, operationalReportConfig } from "./OperationalReportActionScreen.js";
+import { DocumentHistoryActionScreen, documentHistoryConfig } from "./DocumentHistoryActionScreen.js";
 
 /** Public ActionScreen boundary for metadata-selected operational renderers. */
 export function ActionScreen(props: ActionScreenProps) {
   const action = preferFirstClassActionInputTables(props.action);
   if (selectionBatchConfig(action)) return <SelectionBatchActionScreen {...props} action={action} />;
   if (masterDetailListConfig(action)) return <MasterDetailListActionScreen {...props} action={action} />;
+  if (operationalReportConfig(action)) return <OperationalReportActionScreen {...props} action={action} />;
+  if (documentHistoryConfig(action)) return <DocumentHistoryActionScreen {...props} action={action} />;
   return isRichAction(action)
     ? <RichActionScreen {...props} action={action} />
     : <ExistingActionScreen {...props} action={action} />;
